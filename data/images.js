71195398f9b07543694ceeca6520d910db75672b
Somayeh Soleimani <ssoleimani@inolitix.com>
2018-07-10 13:21:30 -0500

    milestone images

---
+++ data/images.js
@@ -0,0 +1,14 @@
+export const imagesData = [
+    { image: require('../assets/images/milestoneImages/img1.png') },
+    { image: require('../assets/images/milestoneImages/img2.png') },
+    { image: require('../assets/images/milestoneImages/img3.png') },
+    { image: require('../assets/images/milestoneImages/img4.png') },
+    { image: require('../assets/images/milestoneImages/img5.png') },
+    { image: require('../assets/images/milestoneImages/img6.png') },
+    { image: require('../assets/images/milestoneImages/img7.png') },
+    { image: require('../assets/images/milestoneImages/img8.png') },
+    { image: require('../assets/images/milestoneImages/img9.png') },
+    { image: require('../assets/images/milestoneImages/img10.png') },
+    { image: require('../assets/images/milestoneImages/img11.png') }
+    
+];
