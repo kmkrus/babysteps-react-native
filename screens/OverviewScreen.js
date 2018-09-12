a15c3a0cb090c1d7a2ecf9637a1f8d96ed1b64bc
Mike Biang <mike.biang@gmail.com>
2018-09-12 14:39:24 -0400

    A few overview screen style updates

--- screens/OverviewScreen.js
+++ screens/OverviewScreen.js
@@ -106,7 +106,7 @@ class OverviewScreen extends React.Component {
     return (
       <View style={styles.screening_slide_container}>
         <Text numberOfLines={1} style={styles.screening_title}>{ item.data.title }</Text>
-        <Text numberOfLines={1} style={styles.screening_date}> { date }</Text>
+        <Text numberOfLines={1} style={styles.screening_date}>{ date }</Text>
         <Text numberOfLines={3} style={styles.screening_text}>{ item.data.message }</Text>
         <View style={styles.screening_slide_link}>
           <TouchableOpacity key={item._pageIndex} style={styles.screening_button}>
@@ -137,15 +137,15 @@ class OverviewScreen extends React.Component {
 
   render() {
     const milestoneGroups = _.sortBy(
-      _.filter(this.props.milestones.groups.data, mg => (mg.visible > 0) ), mg => mg.position 
+      _.filter(this.props.milestones.groups.data, mg => (mg.visible > 0) ), mg => mg.position
     )
 
     return (
-      <ScrollView 
+      <ScrollView
         contentContainerStyle={styles.container}
         showsVerticalScrollIndicator={false}>
-        <ScrollView 
-          style={styles.container} 
+        <ScrollView
+          style={styles.container}
           contentContainerStyle={styles.contentContainer}
         >
           <View style={styles.welcomeContainer}>
@@ -174,7 +174,6 @@ class OverviewScreen extends React.Component {
             <ViewPager
               data={this.props.milestones.calendar.data}
               renderPage={item => this.renderScreeningItem(item)}
-              pageWidth={scSliderWidth}
               renderAsCarousel={false}
             />
           </View>
@@ -186,7 +185,7 @@ class OverviewScreen extends React.Component {
               <Text style={styles.slider_title_text}>Developmental Milestones</Text>
             </View>
             <TouchableOpacity
-              style={styles.opacityStyle} 
+              style={styles.opacityStyle}
               onPress={()=>{this.props.navigation.navigate('Milestones')}} >
               <Text style={styles.slider_link_text}>View all</Text>
               <Ionicons name='md-arrow-forward' style={styles.slider_link_icon} />
@@ -196,7 +195,6 @@ class OverviewScreen extends React.Component {
             <ViewPager
               data={milestoneGroups}
               renderPage={item => this.renderMilestoneItem(item)}
-              pageWidth={mgSliderWidth}
               renderAsCarousel={false}
             />
           </View>
@@ -240,15 +238,13 @@ const styles = StyleSheet.create({
   },
   slide_item: {
     flex: 1,
-    width: mgImageWidth,
     height: mgImageHeight,
     borderRadius: 5,
-    marginLeft: mgImageMargin,
+    width: '100%'
   },
   slide_item_image: {
     flex: 1,
-    width: mgImageWidth,
-    height: mgImageHeight,
+    width: '100%'
   },
   slide_item_footer: {
     position: 'absolute',
@@ -287,16 +283,15 @@ const styles = StyleSheet.create({
   },
   slider: {
     flex: 1,
-    paddingLeft: 5,
     marginBottom: 10,
   },
   mg_touchable: {
     height: mgImageHeight,
   },
   screening_slide_container: {
-    width: scCardWidth,
+    marginLeft: 10,
+    marginRight: 10,
     height: scCardHeight,
-    marginLeft: scCardMargin,
     borderRadius: 5,
     borderColor: Colors.lightGrey,
     borderWidth: 1,
@@ -313,23 +308,27 @@ const styles = StyleSheet.create({
     fontWeight: '900',
   },
   screening_date: {
-    fontSize: 10,
-    color: Colors.darkGrey,
-  },
-  screening_text: {
     fontSize: 12,
     color: Colors.darkGrey,
   },
+  screening_text: {
+    fontSize: 14,
+    marginBottom: 6,
+    marginTop: 6,
+    color: Colors.darkGrey,
+  },
   screening_button: {
-    padding: 3,
-    borderWidth: 1,
+    padding: 8,
+    width: 100,
+    borderWidth: 2,
     borderColor: Colors.pink,
     backgroundColor: Colors.lightPink,
     borderRadius: 5,
   },
   screening_button_text: {
     fontSize: 12,
-    color: Colors.darkPink,
+    color: Colors.pink,
+    textAlign: 'center',
   },
 });
 
