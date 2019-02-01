de5fee088a626e0ad29c682734d2a27f4682f8f7
Mike Biang <mike.biang@gmail.com>
2019-02-01 16:39:07 -0600

    Fix duplicate api calendar requests

--- screens/OverviewScreen.js
+++ screens/OverviewScreen.js
@@ -47,7 +47,12 @@ class OverviewScreen extends React.Component {
 
   componentWillMount() {
     this.props.fetchSubject();
-    this.props.resetApiMilestoneCalendar();
+    //this.props.resetApiMilestoneCalendar(); // I am commenting this out because of the following:
+    //  1. After the last registration form is finished and the subject is registered, apiCreateMilestoneCalendar is called.
+    //  2. The overview screen then loads, and the above this.props.resetApiMilestoneCalendar is called, which resets
+    //     milestone.api_calendar.fetching to false
+    //  3. overview_screening_events.js checks to see if we have calendar data, and if milestone.api_calendar.fetching is true.
+    //  4. milestone.api_calendar.fetching is false because we reset it above (step 2), so a new request to apiCreateMilestoneCalendar is issued.
     this.props.fetchMilestoneCalendar();
     this.props.resetApiMilestones();
     this.props.fetchMilestoneGroups();
