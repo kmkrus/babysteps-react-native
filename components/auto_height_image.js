b98554784baa0f685a19b42cd60ebbc9af1a06b7
Mike Biang <mike.biang@gmail.com>
2019-05-03 17:11:24 -0500

    Fix autoheight image on android

--- components/auto_height_image.js
+++ components/auto_height_image.js
@@ -33,10 +33,10 @@ class AutoHeightImage extends Component {
 
   updateDimensionState = (xWidth, xHeight) => {
     let aspectRatio = xHeight / xWidth;
-    if (Platform.OS === 'ios') {
+    //if (Platform.OS === 'ios') {
       // android and ios are returning width & height in reverse
       aspectRatio = xWidth / xHeight;
-    }
+    //} //I commented out the above block as my tests on android (Samsung Galaxy S7 return in the same order as iOS.)
     const width = this.props.width;
     const height = width * aspectRatio;
     this.setState({ width, height });
@@ -45,6 +45,7 @@ class AutoHeightImage extends Component {
   render = () => {
     const { width, height } = this.state;
     const { source, style } = this.props;
+    console.log("Autoheight Image", width, height)
     return (
       <Image
         source={source}
