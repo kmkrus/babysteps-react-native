aad7e84add1bf2591fbb67913c5720e6c2c3f8b0
kmkrus <68306896+kmkrus@users.noreply.github.com>
2020-07-14 21:22:03 -0500

    Update consent_disclosure_content_002.js
    I've only updated through line 268 and not completely sure if I've formatted everything correctly but will send these questions in an email. I'm happy to go back and redo it if I didn't do it correctly!

--- components/consent_disclosure_content_002.js
+++ components/consent_disclosure_content_002.js
@@ -34,8 +34,7 @@ class ConsentDisclosureContent002 extends Component {
             color={Colors.iconDefault}
           />
           <Text style={styles.header}>
-            Assessment of Social Environment: A Pilot Study using Smart Phone
-            Technology
+            Epigenetics and the Social Environment in Autism: A Cross-Sectional Study
           </Text>
           <Text style={styles.text}>
             <Text style={styles.bold}>Principal Investigator:</Text>{'\n'}
@@ -46,11 +45,11 @@ class ConsentDisclosureContent002 extends Component {
             <Text style={styles.bold}>Research Team Contacts:</Text>
             {'\n'}
             <Text style={styles.bullet}>{'\u2022'} </Text>
-            <Text style={styles.listText}>Guifeng Xu MD (319 356 7044)</Text>
+            <Text style={styles.listText}>Emese Chmielewski BA (216 338 1370)</Text>
             {'\n'}
             <Text style={styles.bullet}>{'\u2022'} </Text>
             <Text style={styles.listText}>
-              Carol Mertens PhD (319 356 7044)
+              Tessa Meisner BAN RN (319 594 6639)
             </Text>
             {'\n'}
             <Text style={styles.bullet}>{'\u2022'} </Text>
@@ -62,8 +61,8 @@ class ConsentDisclosureContent002 extends Component {
             This consent form describes the research study to help you decide if
             you want to participate. This form provides important information
             about what you will be asked to do during the study, about the risks
-            and benefits of the study, and about your rights as a research
-            subject.
+            and benefits of the study, and about yours and your child's rights as research
+            subjects.
           </Text>
           <Text style={styles.text}>
             <Text style={styles.bullet}>{'\u2022'} </Text>
@@ -79,7 +78,7 @@ class ConsentDisclosureContent002 extends Component {
             <Text style={styles.bullet}>{'\u2022'} </Text>
             <Text style={styles.listText}>
               Do not agree to participate in this study unless the research team
-              has answered your questions and you decide that you want to be
+              has answered your questions and you decide that you want you and your child to be
               part of this study.
             </Text>
           </Text>
@@ -94,18 +93,15 @@ class ConsentDisclosureContent002 extends Component {
           />
           <Text style={styles.header}>What is the Purpose of this Study?</Text>
           <Text style={styles.text}>
-            This is a research study. We are inviting you to participate in this
-            research study because you are either pregnant or the mother of a
-            child under 2 years of age. In this study both you and your child
+            This is a research study. We are inviting you and your child to participate in this
+            research study because your child, who may or may not have behavioral or developmental challenges, is 2 to 3 years old. In this study, both you and your child
             will be considered research participants.
           </Text>
           <Text style={styles.text}>
-            The purpose of this research study is to test a new smart phone
-            application called “BabySteps”, which is designed to help pregnant
-            women and new mothers preserve the memories of their pregnancy and
-            newborn’s experience, while also collecting information to help us
-            to better understand risk factors for autism and other developmental
-            disorders of early childhood.
+            The purpose of this research study is to better understand
+            how genetics and the social environment influence the way children develop.
+            Specifically, this study will use a smartphone application and blood samples 
+            to study potential risk factors for Autism Spectrum Disorder or other developmental disorders.
           </Text>
         </View>
 
@@ -118,7 +114,7 @@ class ConsentDisclosureContent002 extends Component {
           />
           <Text style={styles.header}>How Many People Will Participate?</Text>
           <Text style={styles.text}>
-            Approximately 80 mothers and their children will take part in this
+            Approximately 300 mothers and their children will take part in this
             study conducted by investigators at the University of Iowa.
           </Text>
         </View>
@@ -132,13 +128,11 @@ class ConsentDisclosureContent002 extends Component {
           />
           <Text style={styles.header}>How Long Will I Be in this Study?</Text>
           <Text style={styles.text}>
-            If you agree to take part in this study, your involvement will last
-            for up to 3-months, during which time you may be asked to answer
-            simple questions about yourself, your pregnancy or your baby’s
-            development; or to videotape yourself playing with your child. These
-            tasks will appear on your cell phone several times per week. They
-            can be completed at any convenient time for you and should take
-            between 1 and 10 minutes of your time on each occasion.
+            If you agree to take part in this study, you/your child's involvement will last
+            for about 10 months. This may include one or two 15-minute in-person visit(s) 
+            to the Center for Disabilities and Development (CDD), and your continued use of the
+            smartphone app, 'BabySteps' from your home. Completing a prompt or task from the app
+            should only take a few minutes of your time on each occasion. 
           </Text>
         </View>
 
@@ -151,75 +145,127 @@ class ConsentDisclosureContent002 extends Component {
           />
           <Text style={styles.header}>What Will Happen During this Study?</Text>
           <Text style={styles.text}>
-            Before beginning any study procedures and/or signing the study
-            consent, you will have participated in a phone or web-based
-            screening interview to determine your eligibility to participate in
-            the study. If you were eligible, a link to download this smart phone
-            app would have been sent to the email address you provided.
+            Once your eligibility is discussed, we will e-mail a link to access the BabySteps smartphone
+            application. After you have reviewed the BabySteps app, you can press the "Agree" button on the 
+            application to formally consent to the study, and can sign electronically. You and your child cannot
+            participate in this study without giving signed permission. Once the consent form is properly completed,
+            you/your child may complete other app-based tasks and in-person procedures.
           </Text>
           <Text style={styles.text}>
-            If you are still interested in this study after reading through the
-            study description, you can press the “Agree” button as a form of
-            consent, and then sign electronically.
+            During this study, each mother-child pair may meet with researchers once or twice. 
+            If your child has an appointment scheduled at the CDD or another University of Iowa clinic,
+            study researchers may meet with you shortly before this appointment. A blood draw may be part of 
+            the standard care for these appointments, and researchers may request access to a small amount
+            of the blood that is drawn at that time. If your child is not scheduled for a clinical evaluation, or was 
+            scheduled for evaluation but did not have blood drawn at that time, researchers will contact
+            you to explain the procedures for a blood sample collection for your child. This sample can be collected
+            at the CDD by healthcare staff or by you in your home. These blood samples will be compared to a sample
+            of blood that was taken when your child was born (those samples are obtained from all babies born in the 
+            State of Iowa). Analyzing the two samples will reveal whether chemical ("epigenetic") changes occurred over time. 
           </Text>
           <Text style={styles.text}>
-            During this study, you will be asked to answer some simple questions
-            about your feelings during your pregnancy (if pregnant) or as a
-            parent. You are free to skip any questions that you would prefer not
-            to answer. These questions will include:
-          </Text>
-
-          <View style={styles.list}>
-            <Text style={styles.bullet}>{'\u2022'}</Text>
-            <Text style={styles.listText}>
-              feelings that you are experiencing, such as stress or depression
-            </Text>
-          </View>
-          <View style={styles.list}>
-            <Text style={styles.bullet}>{'\u2022'}</Text>
-            <Text style={styles.listText}>
-              thoughts about your pregnancy and motherhood
-            </Text>
-          </View>
-          <View style={styles.list}>
-            <Text style={styles.bullet}>{'\u2022'}</Text>
-            <Text style={styles.listText}>health issues or concerns</Text>
-          </View>
-
-          <Text style={styles.text}>
-            If your baby has already been born, you will be asked to videotape
-            yourself with your baby, or take photos to capture special
-            milestones like your baby’s first words or steps. These pictures and
-            audio clips will only be used within our research studies, or with
-            investigators with whom we collaborate.
+            Researchers will also orient your to the smartphone application, BabySteps, that you will be asked to use 
+            for about 10 months. Within the app, you will be able to take photos and record special milestones.
+            Occasionally, you will be asked to videotape yourself playing with your child. You will also complete 
+            screening tests or questions designed to assess your child's language, motor, and cognitive development.
+            Some of these screening tests will assess for early signs of autism. Certain results may indicate your child
+            is not eligible to continue in this study. If this occurs, you will be notified. You are fee to continue 
+            use of the app, if you so choose. 
           </Text>
           <Text style={styles.text}>
-            In addition, you will receive screening tests for your baby’s
-            language, motor, and cognitive development, as well as to screen for
-            early signs of autism. If your child screens positive for a
-            developmental concern, you will be referred for further assessment.
-            Children who screen positive for autism may have a tele-health
-            assessment or be referred to the Center for Disabilities and
-            Development for additional testing by a developmental pediatrician.
-            Support from the Regional Autism Assistance Program (RAP) will also
-            be provided.
+            We may ask other questions about your mood, your feelings, day-to-day life, or your history.
+            You are free to skip any questions that your would prefer not to answer. Prompts will appear
+            on your cell phone several times per week and will usually only require a single button press. 
+            You should try to answer these as soon as you see them. Other prompts should take between 1 and 10 
+            minutes to complete on occasion. If any of the information gathered through the app, such as information
+            about your well-being or your child's development, indicates a need for intervention, you may be 
+            referred to appropriate providers. 
           </Text>
           <Text style={styles.text}>
-            For mothers with children, we are asking your consent to access your
-            baby’s newborn screening blood spots for genetic testing purposes
-            (blood spots are obtained from all babies born in the State of
-            Iowa). You are requested to give permission to use the blood spot in
-            this research study.
+            You are free to share content stored in the app to your social media accounts. 
+            Study researchers and collaborators will only use your data and content for the 
+            purposes of this study unless you give consent for it to be shared 
+            with other researchers. If your child was assessed in the CDD, we will 
+            also obtain information from your child's medical record about the results 
+            of the evaluation by the doctor, psychologist, and/or speech therapist. 
           </Text>
           <Text style={styles.text}>
-            Your sample, information, and/or data may be stripped of identifiers
-            (such as name, date of birth, address, etc.) and placed in a central
-            repository or other national repositories sponsored by the National
-            Institutes of Health or other Federal agencies. Only qualified
-            researchers who obtain proper permission may gain access to your
-            sample and/or data for use in approved research studies.
+            As part of this study, we are obtaining blood samples from your child.
+            We and other researchers may wish to study your child's blood samples 
+            in the future, after this study witout further consent. These samples,
+            information, and/or data may be placed in a central repository or other 
+            national repositories sponsored by the National Institutes of Health or 
+            other Federal agensies. If this happnes, it may be stripped of identities 
+            (such as name, date of birth, address, etc.). Other qualified researchers
+            who obtain proper permission may gain access to your sample and/or data 
+            for use in approved research studies that may or may not be related to the 
+            purpose of this study.
           </Text>
-
+          <Text style={styles.text}>
+            Blood cells removed from the blood samples may be used to make a cell line 
+            and DNA or conduct whole genome sequencing. Cell lines are produced by growing
+            blood cells in a laboratory and allow us to have a source of the DNA without 
+            having to redraw your blood. These blood cells can be stored for decades or
+            more. The DNA and data might be available to researchers trying to learn more 
+            about the cause of diseases. 
+          </Text>
+          <Text style={styles.text}>
+            Each of the cells in your body contains DNA. DNA is the isntruction manual
+            that determines your appearance in things like eye color or how tall you
+            can be. Your DNA may also lead to higher or lower risk of certain diseases.
+            Your environment will also determine some of your disease risk.
+          </Text>
+          <Text style={styles.text}>
+            Your DNA is a string of four building blocks, called "bases." These bases
+            are represented by the letters arranged in packages like words. Each of these 
+            "words" have specific jobs in the body. Most of the time, the letters are the 
+            same in everyone. But about 1% of the population might have an "A" where 
+            someone else has a "G." This difference can explain why some people have blue
+            eyes and others brown eyes, or why some have a high risk for a certain cancer
+            and others a low risk. All these letters come together to create your 
+            "genome sequence," a kind of book of your genetics. It is now possible to 
+            read off each of these letters and read your complete genome sequence. 
+            Your DNA sequence is unique to you. You inherit your DNA in almost equal parts
+            from each of your parents. In very rare cases, your genome can also change 
+            through "mutations." A mutation is like if you tried to copy a page from a 
+            book, but misspelled some words. Mutations usually result from copying errors 
+            in certain letters when being passed from parent to child.            
+          </Text>
+          <Text style={styles.text}>
+            When we take a sample of your child's blood for this study, it might go
+            to a lab to read off those letters and give us a report on your child's
+            genome. This information will then be compared with other genomes to see
+            how they may be the same or different. It is our hope that this will help
+            us to better understand how the human body works and/or what causes it 
+            to not work well, as when someone has a disease. 
+          </Text>          
+          <Text style={styles.text}>
+            The tests we might want to use to study your child's blood samples may
+            not even exist at this time. Therefore, we are asking for your permisison
+            to store your child's blood samples so that we can study them in the 
+            future. These future studies may provide additional information that will 
+            be helpful in understanding Autism Spectrum Disorder, but it is unlikely
+            that what we learn from these studies will have a direct benefit to you
+            or your child. It is possible that your child's blood samples might be 
+            used to develop products, tests, or discoveries that could be patented
+            and licensed. In some instances, these may have potential commercial
+            value and may be developed by the Investigators, University of Iowa,
+            commercial companies, funding organization, or others that may not be 
+            working directly with this research team. However, donors of blood
+            samples do not retain any property rights to the materials. Therefore, 
+            there are no plans to provide financial compensation to you should
+            this occur. 
+          </Text>
+          <Text style={styles.text}>
+            Your child's blood samples will be stored with a code which may be 
+            linked to your/your child's identifying information, such as name
+            or date of birth. If you agree now to future use of your child's 
+            blood sample but decide in the future that you would like to have 
+            it removed from future research, you should contact Lane Strathearn,
+            MD PhD (319-356-7044). However, if some researach with your child's
+            blood sample has already been completed, the information from that 
+            resarch may still be used.                 
+          </Text>
           <Text style={styles.subTitle}>Genetic Research</Text>
           <Text style={styles.text}>
             One purpose of this study is to look at genes (DNA) and how they
